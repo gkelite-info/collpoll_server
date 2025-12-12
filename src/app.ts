807f2d06c48de508c8fcb09eda10afbc2242374e
gkelite-info <gkeliteinfo@gmail.com>
2025-12-12 18:41:45 +0530

    a

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv'
-import dbInit from './db/init';
-import routes from './db/routes';
 dotenv.config();
+import dbInit from './db/init';
 
 const app = express();
 const port = process.env.PORT;
 
 dbInit();
-
-// app.use("/api/v1", routes);
 
 app.get('/', (req, res) => {
     res.send("Heyy collpoll");
